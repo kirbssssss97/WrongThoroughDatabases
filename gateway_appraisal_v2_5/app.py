a15ce96d26ead059565e2622a6c3208587ea1e53
kirbssssss97 <froth_sierra9x@icloud.com>
2025-08-07 21:45:11 -0500

    Update app.py

--- gateway_appraisal_v2_5/app.py
+++ gateway_appraisal_v2_5/app.py
@@ -1,6 +1,10 @@
-# Sample Flask app
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="templates", static_folder="static")
+
+@app.route("/")
 def home():
-    return 'Hello from Gateway Appraisal v2.5'
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
